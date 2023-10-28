2204335154063bf0da1aca9c1223bdef4cf4cf83
iandk <hi@ianklemm.de>
2023-10-28 22:15:45 +0000

    add caching

--- actions/initial_setup.sh
+++ actions/initial_setup.sh
@@ -7,4 +7,13 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --d
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 apt install -y caddy
 mysql_secure_installation
+
+echo "# Custom config
+opcache.interned_strings_buffer=8
+opcache.max_accelerated_files=100000
+opcache.memory_consumption=512
+opcache.save_comments=1
+opcache.revalidate_freq=1" >> /etc/php/8.2/fpm/php.ini
+service php8.2-fpm restart
+
 touch /opt/.easyweb
